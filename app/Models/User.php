96786f5e2bd658583344586dae5f0003d7ea03fb
hieupc-0248 <phan.cong.hieu@sun-asterisk.com>
2023-08-29 15:44:22 +0700

    task3

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -48,8 +49,13 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function tasks() 
+    public function task(): HasMany
     {
-        return $this->HasMany(Task::class);
+        return $this->hasMany(Task::class);
+    }
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
     }
 }
